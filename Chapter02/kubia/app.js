c6a37f3c94f198c77cff91e1a5dde4cc1431408e
Solip Kim <156478595+120618194-myseneca@users.noreply.github.com>
2024-01-26 00:43:14 -0500

    Update app.js

--- Chapter02/kubia/app.js
+++ Chapter02/kubia/app.js
@@ -5,10 +5,18 @@ console.log("Kubia server starting...");
 
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
-  response.writeHead(200);
-  response.end("You've hit " + os.hostname() + "\n");
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  // Check if a version environment variable is set, default to 'default'
+  const version = process.env.VERSION || 'default';
+
+  response.end(`You've hit ${os.hostname()} - Version: ${version}\n`);
 };
 
-var www = http.createServer(handler);
-www.listen(8080);
+// Get the desired port from the command line arguments or default to 8080
+const port = process.argv[2] || 8080;
 
+var www = http.createServer(handler);
+www.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
